2e5b3e12cbec7edc3e65e865c9e394cf20c4341e
zhaohy <zhaohy32196@163.com>
2018-12-05 16:27:45 +0800

    1111111111111111

---
+++ dugujiujian1/src/main/java/com/coo/jianfa/Dugujiujian.java
@@ -0,0 +1,14 @@
+package com.coo.jianfa;
+
+/**
+ * @author shkstart
+ * @date 2018/12/5 - 15:08
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+    }
+}
